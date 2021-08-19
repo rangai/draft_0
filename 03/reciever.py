379c72b7a34dc53ef9c391bf1e26017051e93094
rangai <fibo.112358132134@gmail.com>
2021-08-19 21:44:02 +0900

    typo correction

--- 03/reciever.py
+++ 03/reciever.py
@@ -5,7 +5,7 @@ s.connect(('8.8.8.8', 80))
 my_host=s.getsockname()[0]
 print(my_host)
 my_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-my_port=int(input("my_port:"))
+my_port=int(input('my port:'))
 my_socket.bind((my_host, my_port))
 my_socket.listen()
 soc, addr = my_socket.accept()
