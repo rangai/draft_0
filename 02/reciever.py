8016d6553fade8196e09110df98d497ca06888b5
rangai <fibo.112358132134@gmail.com>
2021-08-19 21:26:19 +0900

    02

---
+++ 02/reciever.py
@@ -0,0 +1,13 @@
+import socket
+
+s=socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+s.connect(('8.8.8.8', 80))
+my_host=s.getsockname()[0]
+print(my_host)
+my_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+my_port=int(input("my_port:"))
+my_socket.bind((my_host, my_port))
+my_socket.listen()
+conn, addr = my_socket.accept()
+print(conn)
+print(addr)
