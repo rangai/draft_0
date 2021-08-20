f0bc30ae55a717de464585b81e76cbf9e9bd9402
rangai <fibo.112358132134@gmail.com>
2021-08-20 23:48:40 +0900

    04/hello and thank you

---
+++ 04/thank_you.py
@@ -0,0 +1,15 @@
+import socket
+
+my_host='127.0.0.1'
+my_port=50090
+the_other_host='127.0.0.1'
+the_other_port=50092
+
+my_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+my_socket.bind((my_host, my_port))
+
+my_socket.listen()
+soc, addr = my_socket.accept()
+content=soc.recv(4096)
+print(content)
+soc.sendall('thank you'.encode('utf-8'))
