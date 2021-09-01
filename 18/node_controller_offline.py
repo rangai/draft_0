aa3ac049da8ad542e66e33d0a1b4764dbead97b5
rangai <fibo.112358132134@gmail.com>
2021-09-01 22:28:35 +0900

    type correction

--- 18/node_controller_offline.py
+++ 18/node_controller_offline.py
@@ -6,14 +6,14 @@ from blockchain import TxPool
 tp = TxPool()
 
 while True:
-    cmd = int(input('cmd'))
+    cmd = int(input('cmd:'))
     if cmd == 0:
         txs = tp.get_txs()
         print(txs)
     elif cmd == 1:
-        sender = input('sender')
-        receiver = input('receiver')
-        value = int(input('value'))
+        sender = input('sender:')
+        receiver = input('receiver:')
+        value = int(input('value:'))
         t = {'sender':sender, 'receiver':receiver, 'value':value}
         tp.add_tx(t)
     elif cmd == 2:
