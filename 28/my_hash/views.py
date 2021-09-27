9d17aed3731b2de5fc5e7bfec2bcc2f1df3196ea
rangai <fibo.112358132134@gmail.com>
2021-09-27 20:41:59 +0900

    28 fixed

--- 28/my_hash/views.py
+++
@@ -1,6 +0,0 @@
-from django.shortcuts import render
-from .models import MyHash
-
-def my_hash_list(request):
-    hashes = MyHash.objects.all().order_by('message')
-    return render(request, 'my_hash/my_hash_list.html', {'hashes':hashes})
--- 28/my_hash_site/my_hash/urls.py
+++ 28/my_hash_site/my_hash/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.my_hash_list, name='my_hash_list'),
+    path('my_hash/new/', views.my_hash_new, name='my_hash_new'),
 ]
